36d146ecaf4f1f5e0e402e925b06760896ca97b2
KD4-shivani-83519 <shivanidpatil11@gmail.com>
2025-08-18 12:31:37 +0530

    login- feture

---
+++ backend/logsheet/src/main/java/com/sunbeam/logsheet/entity/Role.java
@@ -0,0 +1,28 @@
+package com.sunbeam.logsheet.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String title;
+
+    
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
