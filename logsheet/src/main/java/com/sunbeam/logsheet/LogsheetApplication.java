b64a54de5decbdeb78369ea30749ea8684512525
KD4-Balkisan-83600 <balkishanvishwakarma94@gmail.com>
2025-08-18 08:09:12 +0530

    general template

---
+++ logsheet/src/main/java/com/sunbeam/logsheet/LogsheetApplication.java
@@ -0,0 +1,28 @@
+package com.sunbeam.logsheet;
+
+import org.modelmapper.Conditions;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class LogsheetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogsheetApplication.class, args);
+	}
+	
+	
+	@Bean // equivalent to <bean id ..../> in xml file
+	public ModelMapper mapper() {
+		ModelMapper modelMapper = new ModelMapper();
+		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT)
+	    .setPropertyCondition(Conditions.isNotNull());
+		return modelMapper;
+	}
+
+}
+
+
