bac78ff2d07b07dca1e3afa4ef5976c5bf3bfdb1
kd4snehal-83553 <snehalgaikwad4656@gmail.com>
2025-08-20 17:59:17 +0530

    apiresponse added in premises

--- backend/logsheet/src/main/java/com/sunbeam/logsheet/repository/PremisesRepository.java
+++ backend/logsheet/src/main/java/com/sunbeam/logsheet/repository/PremisesRepository.java
@@ -1,7 +1,11 @@
 package com.sunbeam.logsheet.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.sunbeam.logsheet.entity.Premises;
 
 public interface PremisesRepository extends JpaRepository<Premises, Long> {
+
+	Optional<Premises> findByTitle(String title);
 }
