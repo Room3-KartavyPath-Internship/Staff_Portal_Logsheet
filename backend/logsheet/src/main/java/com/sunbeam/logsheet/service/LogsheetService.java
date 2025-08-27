ecfc5a955301def4535dfd4f362d9843ecd91069
KD4-Balkisan-83600 <balkishanvishwakarma94@gmail.com>
2025-08-27 10:19:28 +0530

    modified logsheet

--- backend/logsheet/src/main/java/com/sunbeam/logsheet/service/LogsheetService.java
+++ backend/logsheet/src/main/java/com/sunbeam/logsheet/service/LogsheetService.java
@@ -6,10 +6,10 @@ import com.sunbeam.logsheet.DTO.ApiResponse;
 import java.util.List;
 
 public interface LogsheetService {
-    ApiResponse<LogsheetDTO> addLogsheet(LogsheetDTO dto);
+    ApiResponse<LogsheetDTO> addLogsheet(LogsheetCreateDTO dto);
     ApiResponse<List<LogsheetDTO>> getAllLogsheets();
     ApiResponse<LogsheetDTO> getLogsheetById(Long id);
-    ApiResponse<LogsheetDTO> updateLogsheet(Long id, LogsheetDTO dto);
+    ApiResponse<LogsheetDTO> updateLogsheet(Long id, LogsheetUpdateDTO dto);
     ApiResponse<Boolean> deleteLogsheet(Long id);
 
     ApiResponse<LogsheetDTO> verifyLog(Long id, VerifyLogRequest request);
